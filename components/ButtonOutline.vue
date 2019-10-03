8ccb6f10aee8bf1e309ee06f2bbc887c32b9e639
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-10-03 22:56:32 +0200

    faster transition (normal) ←→ :hover

--- components/ButtonOutline.vue
+++ components/ButtonOutline.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-    button(:name="name", :id="name ? `btn--${name}` : false"): slot
+    button(:name="name", :id="name ? `btn--${name}` : false")
+      nuxt-link(:to="to" v-if="to")
+        slot
+      template(v-else)
+        slot
 </template>
 
 <script>
@@ -9,6 +13,10 @@ export default {
     name: {
       type: [String, Boolean],
       default: false
+    },
+    to: {
+      type: [String, Boolean],
+      default: ''
     }
   }
 }
@@ -21,16 +29,22 @@ button {
   border: 2px solid white;
   border-radius: 0;
   padding: 10px 20px;
-  color: white;
+  &, a {
+    color: white;
+    text-decoration none
+  }
   font-family: 'Narita Monospace';
   font-size: 20px;
   // ---------------------------------------------------
-  transition: all 0.25s ease;
+  transition: all 0.125s ease;
 }
 
 button:hover {
   background: white;
   color: black;
   cursor: pointer;
+  a {
+    color black
+  }
 }
 </style>
